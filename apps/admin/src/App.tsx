b4577d570e20a2b0f4770170a501984605d19915
adrians5j <adrian@webiny.com>
2022-08-30 11:55:27 +0200

    chore: add frontend examples

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Admin, /*AddLogo*/ } from "@webiny/app-serverless-cms";
-import { Cognito } from "@webiny/app-admin-users-cognito";
 import "./App.scss";
-// import { AddMenu, AddRoute, Plugins } from "@webiny/app-admin";
+import { Admin /*Plugins, AddMenu, AddRoute,*/ /*AddLogo*/ } from "@webiny/app-serverless-cms";
+import { Cognito } from "@webiny/app-admin-users-cognito";
 // import { AdminLayout } from "@webiny/app-admin/components/AdminLayout";
 // import Helmet from "react-helmet";
 // import { LeftPanel, RightPanel, SplitView } from "@webiny/app-admin/components/SplitView";
